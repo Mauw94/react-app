f9811111ddbe18dd557f54dd5c45b104bccdc11d
Maurits Seelen <mauritsseelen@gmail.com>
2017-10-29 16:44:51 +0100

    fixed merge conflicts

--- my-app/src/probleemmeldingen/probleemmelding.details.js
+++ my-app/src/probleemmeldingen/probleemmelding.details.js
@@ -16,37 +16,37 @@ class ProbleemmeldingDetailsPage extends React.Component {
         const id = this.props.match.params.id;
         const locatieid = this.props.match.params.locatieid;
         console.log(locatieid);
-<<<<<<< HEAD
+
         if (!fetched) {
             HttpService.getProbleemMeldingById(id).then(fetchedEntry => this.props.setEntry(fetchedEntry));
             HttpService.getLocatieById(locatieid).then(fetchedLocatie => this.props.setLocatie(fetchedLocatie));
             fetched = true;
-=======
-        HttpService.getProbleemMeldingById(id).then(fetchedEntry => this.props.setEntry(fetchedEntry));
-        HttpService.getLocatieById(locatieid).then(fetchedLocatie => this.props.setLocatie(fetchedLocatie));
-        HttpService.getScoreByIdProbleemmelding(id).then(fetchedScore => this.props.setScore(fetchedScore));
-        this.checkAfgehandeld();
-        this.berekenScore();
+            HttpService.getProbleemMeldingById(id).then(fetchedEntry => this.props.setEntry(fetchedEntry));
+            HttpService.getLocatieById(locatieid).then(fetchedLocatie => this.props.setLocatie(fetchedLocatie));
+            HttpService.getScoreByIdProbleemmelding(id).then(fetchedScore => this.props.setScore(fetchedScore));
+            this.checkAfgehandeld();
+            this.berekenScore();
+        }
     }
-
 
     checkAfgehandeld() {
         if (this.props.probleemEntry.afgehandeld === 1) {
             this.afgehandeld = 'Ja';
         } else {
             this.afgehandeld = 'Nee'
->>>>>>> master
         }
     }
-    berekenScore(){
+
+    berekenScore() {
         console.log("test");
-        this.score = this.props.scoreEntry.totaleScore/this.props.scoreEntry.aantalScores;
+        this.score = this.props.scoreEntry.totaleScore / this.props.scoreEntry.aantalScores;
     }
 
     render() {
         const fetchedEntry = this.props.probleemEntry;
         const locatieEntry = this.props.locatieEntry;
-<<<<<<< HEAD
+        const scoreEntry = this.props.scoreEntry;
+
         console.log('afgehandeld? ' + this.props.probleemEntry.afgehandeld);
 
         return (
@@ -59,30 +59,13 @@ class ProbleemmeldingDetailsPage extends React.Component {
                         <p style={style}><b>Datum: </b> {fetchedEntry.datum}</p>
                         <p style={style}><b>Afgehandeld: </b> {fetchedEntry.afgehandeld}</p>
                         <Link to={'/problemen'}>
-                            <button className={'mdl-button mdl-js-button mdl-button--raised mdl-button--colored'}>Back
+                            <button className={'mdl-button mdl-js-button mdl-button--raised mdl-button--colored'}>
+                                Back
                             </button>
                         </Link>
                     </div>
                 </form>
             </div>
-=======
-        const scoreEntry = this.props.scoreEntry;
-
-        return (
-            <form>
-                <div className={'form-group'} style={{textAlign: 'center'}}>
-                    <h3>Details of probleem melding: {fetchedEntry.id}</h3>
-                    <p style={style}>Probleem: {fetchedEntry.probleem}</p>
-                    <p style={style}>Locatie: {locatieEntry.naam}</p>
-                    <p style={style}>Datum: {fetchedEntry.datum}</p>
-                    <p style={style}>Afgehandeld: {this.afgehandeld}</p>
-                    <p style={style}>Score: {this.score} </p>
-                    <Link to={'/problemen'}>
-                        <button className={'btn btn-default'}>Back</button>
-                    </Link>
-                </div>
-            </form>
->>>>>>> master
         );
     }
 
@@ -91,27 +74,31 @@ class ProbleemmeldingDetailsPage extends React.Component {
     }
 }
 
-const mapStateToProps = (state, ownProps) => {
-    return {
-        probleemEntry: state.probleemEntry,
-        locatieEntry: state.locatieEntry,
-        scoreEntry: state.scoreEntry,
+const
+    mapStateToProps = (state, ownProps) => {
+        return {
+            probleemEntry: state.probleemEntry,
+            locatieEntry: state.locatieEntry,
+            scoreEntry: state.scoreEntry,
+        };
     };
-};
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-    return {
-        ...mapDispatchTitleToProps(dispatch, ownProps),
-        setEntry: (entry) => {
-            dispatch({type: 'SET_PROBLEEMMELDING_ENTRY', payload: entry});
-        },
-        setLocatie: (locatie) => {
-            dispatch({type: 'SET_LOCATIE_ENTRY', payload: locatie});
-        },
-        setScore:(score)=>{
-            dispatch({type: 'SET_SCORE_ENTRY', payload:score});
+const
+    mapDispatchToProps = (dispatch, ownProps) => {
+        return {
+            ...mapDispatchTitleToProps(dispatch, ownProps),
+            setEntry: (entry) => {
+                dispatch({type: 'SET_PROBLEEMMELDING_ENTRY', payload: entry});
+            },
+            setLocatie: (locatie) => {
+                dispatch({type: 'SET_LOCATIE_ENTRY', payload: locatie});
+            },
+            setScore: (score) => {
+                dispatch({type: 'SET_SCORE_ENTRY', payload: score});
+            }
         }
     }
-}
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProbleemmeldingDetailsPage)
+
+
